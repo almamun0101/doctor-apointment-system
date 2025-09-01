a639f400bbc779ff70e57af1b19027d1d1af5a9c
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-09-01 22:23:18 +0600

    navbar added

---
+++ src/app/Home/page.jsx
@@ -0,0 +1,12 @@
+import { Navbar5 } from '@/components/navbar-5'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar5/>
+    </div>
+  )
+}
+
+export default page
